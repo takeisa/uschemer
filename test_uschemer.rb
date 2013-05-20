56d37573ceaee188a52e629e72233cee8d490350
Satoshi Takei <s.takei.dev@gmail.com>
2013-05-20 21:47:03 +0900

    Refactoring
    Implement evaluator class.

--- test_uschemer.rb
+++ test_uschemer.rb
@@ -4,123 +4,27 @@
 # test code
 
 require "./uschemer"
-require "stringio"
 require "pp"
 
 @env = [USchemeR::KEYWORDS, USchemeR::FUNCS]
 
 def eval_print(string)
   print string
-  parser = Parser.new(Lexer.new(StringIO.new(string)))
-  exp = parser.parse
-# p exp
-# p @env
-  result = USchemeR.eval(exp, @env)
-  print " ;=> "
+  exp = USchemeR.parse_string(string)
+  eval = USchemeR::Eval.new
+  result = eval.eval(exp, @env)
+  print ";=> "
   print PP.pp(result, '')
   print "\n"
 end
 
-def test_parse_stdin
-  print Parser.new(Lexer.new(STDIN)).parse
-end
-
 eval_print("
-(define (abs x)
-  (cond ((< x 0) (- x))
-        ((= x 0) 0)
-        (else x)))
+(define (fact n)
+  (if (= n 0)
+      1
+      (* n (fact (- n 1)))))
 ")
 
 eval_print("
-(abs -10)
+(fact 1)
 ")
-
-eval_print("
-(abs 0)
-")
-
-eval_print("
-(abs 10)
-")
-
-
-# test_parse_stdin
-
-# eval_print("
-# (define (hello) \"こんにちは、世界\")
-# ")
-
-# eval_print("
-# (hello)
-# ")
-
-# eval_print("
-# (define Y
-#   (lambda (f)
-#     ((lambda (g)
-#        (f (lambda (arg) ((g g) arg))))
-#      (lambda (g)
-#        (f (lambda (arg) ((g g) arg)))))))
-# ")
-
-# eval_print("
-# (define fact
-#   (lambda (f)
-#     (lambda (n)
-#       (if (= n 0)
-#           1
-#           (* n (f (- n 1)))))))
-# ")
-
-# eval_print("
-# ((Y fact) 10)
-# ")
-
-# eval_print("
-# (define one 1)
-# ")
-
-# eval_print("
-# one
-# ")
-
-# eval_print("
-# (define (fact n)
-#   (if (= n 0)
-#       1
-#       (* n (fact (- n 1)))))
-# ")
-
-# eval_print("
-# (fact 10)
-# ")
-
-# eval_print("
-# (define (fib n)
-#   (if (<= n 2)
-#       1
-#       (+ (fib (- n 2)) (fib (- n 1)))))
-# ")
-
-# eval_print("
-# (fib 15)
-# ")
-
-# eval_print("
-# (letrec ((fact 
-#          (lambda (x)
-#                  (if (= x 0)
-#                      1
-#                      (* x (fact (- x 1)))))))
-#   (fact 10))
-# ")
-
-# eval_print("
-# (letrec ((fact 
-#          (lambda (x)
-#                  (if (= x 0)
-#                      1
-#                      (* x (fact (- x 1)))))))
-#   (fact 100))
-# ")
--- uschemer.rb
+++ uschemer.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+require 'stringio'
 require './parser'
 
-module USchemeR
-  DEBUG = false
-
+class USchemeR
+  DEBUG = true
+  
   FUNCS = {
     :+ => [:built_in, lambda {|x, y| x + y}],
     :- => [:built_in, lambda {|*x| x.reduce(0) {|a, x| a - x}}],
@@ -17,19 +18,19 @@ module USchemeR
     :<= => [:built_in, lambda {|x, y| x <= y}],
     :>= => [:built_in, lambda {|x, y| x >= y}]
   }
-
+  
   KEYWORDS = {
     :true => true,
     :false => false
   }
-
-  class << self
+  
+  class Eval
     def eval(exp, env)
-if DEBUG then
-  print "eval\n"
-  print "  env=" + PP.pp(env[0..-3], '')
-  print "  exp=" + PP.pp(exp, '')
-end
+      if DEBUG then
+        print "eval\n"
+        print "  env=" + PP.pp(env[0..-3], '')
+        print "  exp=" + PP.pp(exp, '')
+      end
       if list?(exp)
         if special_form?(exp) then
           eval_special_form(exp, env)
@@ -44,7 +45,7 @@ end
         end
       end
     end
-
+    
     def eval_special_form(exp, env)
       if lambda?(exp) then
         eval_lambda(exp, env)
@@ -60,33 +61,33 @@ end
         eval_cond(exp, env)
       end
     end
-
+    
     def eval_lambda(exp, env)
       create_closure(exp, env)
     end
-
+    
     def create_closure(exp, env)
       params, body = lambda_to_params_body(exp)
       [:closure, params, body, env]
     end
-
+    
     def lambda_to_params_body(exp)
       [exp[1], exp[2]]
     end
-
+    
     def eval_let(exp, env)
       params, values, body = let_to_params_values_body(exp)
       new_exp = [[:lambda, params, body], *values]
       eval(new_exp, env)
     end
-
+    
     def let_to_params_values_body(exp)
       bind_list = exp[1]
       params, values = bind_list_to_params_values(bind_list)
       body = exp[2]
       [params, values, body]
     end
-
+    
     def eval_if(exp, env)
       test_form, then_form, else_form = if_to_test_then_else(exp)
       if eval(test_form, env) then
@@ -95,40 +96,40 @@ end
         eval(else_form, env)
       end
     end
-
+    
     def if_to_test_then_else(exp)
       [exp[1], exp[2], exp[3]]
     end
-
+    
     def eval_letrec(exp, env)
       params, values, body = letrec_to_params_values_body(exp)
       closures = values.map {|value| eval(value, env)}
       new_env = extend_env(params, closures, env)
-
+      
       bind_hash = car(new_env)
       closures.each {|closure| extend_closure_env!(closure, bind_hash)}
-
+      
       eval(body, new_env)
     end
-
+    
     def extend_closure_env!(closure, bind_hash)
       closure_env = closure[3]
       closure_env.push(bind_hash)
     end
-
+    
     def letrec_to_params_values_body(exp)
       bind_list = exp[1]
       params, values = bind_list_to_params_values(bind_list)
       body = exp[2]
       [params, values, body]
     end
-
+    
     def bind_list_to_params_values(bind_list)
       params = bind_list.map {|bind| bind[0]}
       values = bind_list.map {|bind| bind[1]}
       [params, values]
     end
-
+    
     def eval_define(exp, env)
       if define_with_param?(exp) then
         var, val = define_with_param_to_var_val(exp)
@@ -139,11 +140,11 @@ end
       define!(var, val, env)
       [var, val]
     end
-
+    
     def define_with_param?(exp)
       list?(exp[1])
     end
-
+    
     def define_with_param_to_var_val(exp)
       var = car(exp[1])
       params = exp[1][1..-1]
@@ -151,13 +152,13 @@ end
       val = [:lambda, params, body]
       [var, val]
     end
-
+    
     def define_to_var_val(exp)
       var = exp[1]
       val = exp[2]
       [var, val]
     end
-
+    
     def define!(var, val, env)
       bind_hash = lookup_bind_hash(var, env)
       if bind_hash.nil? then
@@ -166,16 +167,16 @@ end
         bind_hash[var] = val
       end
     end
-
+    
     def lookup_bind_hash(var, env)
       env.find{|h| h.key?(var)}
     end
-
+    
     def extend_env!(vars, vals, env)
       bind_hash = create_bind_hash(vars, vals)
       env.unshift(bind_hash)
     end
-
+    
     def eval_cond(exp, env)
       pred_exp_list = cond_to_pre_exp_list(exp)
       pred_exp_list.each do |pred_exp|
@@ -186,44 +187,44 @@ end
       end
       raise "cond: not match conditions"
     end
-
+    
     def cond_to_pre_exp_list(exp)
       exp[1..-1]
     end
-
+    
     def special_form?(exp)
       lambda?(exp) || let?(exp) || if?(exp) || letrec?(exp) || 
         define?(exp) || cond?(exp)
     end
-
+    
     def lambda?(exp)
       car(exp) == :lambda
     end
-
+    
     def let?(exp)
       car(exp) == :let
     end
-
+    
     def if?(exp)
       car(exp) == :if
     end
-
+    
     def letrec?(exp)
       car(exp) == :letrec
     end
-
+    
     def define?(exp)
       car(exp) == :define
     end
-
+    
     def cond?(exp)
       car(exp) == :cond
     end
-
+    
     def immidiate_value?(exp)
       number?(exp) || string?(exp)
     end
-
+    
     def lookup_var(exp, env)
       var_hash = env.find {|h| h.key?(exp)}
       if var_hash.nil? then
@@ -231,55 +232,55 @@ end
       end
       var_hash[exp]
     end
-
+    
     def eval_func(exp, env)
-if DEBUG then
-  print "eval_func\n"
-end
+      if DEBUG then
+        print "eval_func\n"
+      end
       func = eval(car(exp), env)
       if func.nil? then
         raise "call nil func: #{exp}"
       end
-
+      
       args = eval_list(cdr(exp), env)
-
+      
       if built_in?(func) then
         func_apply(func, args)
       else
         lambda_apply(func, args)
       end
     end
-
+    
     def built_in?(func)
       func[0] == :built_in
     end
-
+    
     def func_apply(func, args)
       func[1].call(*args)
     end
-
+    
     def lambda_apply(func, args)
       params, body, env = closure_to_params_body_env(func)
       new_env = extend_env(params, args, env)
-if DEBUG then
-  print "lambda_apply\n"
-  print "  new_env=" + PP.pp(new_env[0..-3], '')
-  print "  closure:params=" + PP.pp(params, '')
-  print "  closure:body=" + PP.pp(body, '')
-  print "  closure:env=" + PP.pp(env[0..-3], '')
-end
+      if DEBUG then
+        print "lambda_apply\n"
+        print "  new_env=" + PP.pp(new_env[0..-3], '')
+        print "  closure:params=" + PP.pp(params, '')
+        print "  closure:body=" + PP.pp(body, '')
+        print "  closure:env=" + PP.pp(env[0..-3], '')
+      end
       eval(body, new_env)
     end
-
+    
     def closure_to_params_body_env(func)
       func[1..3]
     end
-
+    
     def extend_env(params, args, env)
       bind_hash = create_bind_hash(params, args)
       [bind_hash] + env
     end
-
+    
     def create_bind_hash(params, args)
       bind_list = params.zip(args)
       bind_hash = {}
@@ -288,31 +289,35 @@ end
       end
       bind_hash
     end
-
+    
     def eval_list(list, env)
       list.map {|item| eval(item, env)}
     end
-
+    
     def list?(exp)
       exp.is_a?(Array)
     end
-
+    
     def number?(value)
       value.is_a?(Numeric)
     end
-
+    
     def string?(value)
       value.is_a?(String)
     end
-
+    
     def car(exp)
       exp[0]
     end
-
+    
     def cdr(exp)
       exp[1..-1]
     end
+  end
 
+  class << self
+    def parse_string(string)
+      Parser.new(Lexer.new(StringIO.new(string))).parse
+    end
   end
 end
-
